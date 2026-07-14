b3e8921136849cdc89c1a63f3b94747ecad3805a
Minhaju Islam <minhajul@gmail.com>
2026-07-15 00:55:21 +0530

    Fix Vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
     return {
-        base: '/', // <-- Add this line
+        base: '/of.owncircles.com/',
 
         plugins: [react(), tailwindcss()],
 
