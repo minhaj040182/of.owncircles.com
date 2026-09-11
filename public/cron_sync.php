72a5dfed9c38d3cf82d49813fc2028b569ec92e7
Minhaju Islam <minhajul@gmail.com>
2026-09-11 18:40:24 +0530

    Update OwnFormatter

--- public/cron_sync.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Main Cron Sync Entrypoint
- * Executes the Hourly Google Trending Products Video Synchronizer and YouTube Collector
- */
-require_once __DIR__ . '/cron_hourly_trending.php';
-?>
-
